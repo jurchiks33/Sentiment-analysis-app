f000caf4e80d4d7c7f2cc6e2e3a53db40d2b011e
jurchiks33 <jurisslisans@gmail.com>
2023-12-16 19:39:06 +0100

    interface

--- VibeScan.py
+++ VibeScan.py
@@ -3,3 +3,13 @@ import nltk
 nltk.download('vader_lexicon')
 nltk.download('punkt')
 
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+def analyze_sentiment(text):
+    sia = SentimentIntensityAnalyzer()
+    sentiment = sia.polarity_scores(text)
+    return sentiment
+
+user_input = input("Enter a sentence for sentiment analysis: ")
+result = analyze_sentiment(user_input)
+print("Sentiment Analysis:", result)
