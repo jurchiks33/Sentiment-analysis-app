794291aef6f2f09eaeac9e4bb966e6ea9a54d776
jurchiks33 <jurisslisans@gmail.com>
2023-12-16 19:46:29 +0100

    work on a text input

--- VibeScan.py
+++ VibeScan.py
@@ -1,15 +1,18 @@
 import nltk
 
-nltk.download('vader_lexicon')
-nltk.download('punkt')
+# nltk.download('vader_lexicon')
+# nltk.download('punkt')
 
 from nltk.sentiment import SentimentIntensityAnalyzer
 
-def analyze_sentiment(text):
+def analyze_sentiment():
+    text = text_input.get("1.0", "end-1c")
     sia = SentimentIntensityAnalyzer()
     sentiment = sia.polarity_scores(text)
-    return sentiment
+    result_label.config(text=str(sentiment))
 
-user_input = input("Enter a sentence for sentiment analysis: ")
-result = analyze_sentiment(user_input)
-print("Sentiment Analysis:", result)
+root = tk.Tk()
+root.title("Sentiment Analysis App")
+
+text_input = tk.Text(root, height=10, width=50)
+text_input.pack()
