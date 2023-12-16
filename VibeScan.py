75ff399333f5cd3ffa3eec483e5db0aa18581b26
jurchiks33 <jurisslisans@gmail.com>
2023-12-16 20:04:19 +0100

    work on a pie display

--- VibeScan.py
+++ VibeScan.py
@@ -1,13 +1,10 @@
-import nltk
 import tkinter as tk
-from tkinter import Toplevel
+from nltk.sentiment import SentimentIntensityAnalyzer
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-# nltk.download('vader_lexicon')
-# nltk.download('punkt')
-
-from nltk.sentiment import SentimentIntensityAnalyzer
+chart = None
+canvas = None
 
 def analyze_sentiment():
     text = text_input.get("1.0", "end-1c")
@@ -20,15 +17,21 @@ def analyze_sentiment():
     sizes = [sentiment['pos'], sentiment['neu'], sentiment['neg']]
     colors = ['green', 'blue', 'red']
 
-    # Create figure for the pie chart
-    fig, ax = plt.subplots()
-    ax.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=140)
-    ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    # Update the pie chart with new data
+    if 'chart' in globals():
+        chart.set_sizes(sizes)
+        canvas.draw_idle()
+    else:
+        # Create figure for the pie chart
+        fig, ax = plt.subplots()
+        global chart, canvas
+        chart = ax.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=140)
+        ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
-    # Embedding the pie chart into the Tkinter window
-    canvas = FigureCanvasTkAgg(fig, master=root)  
-    canvas_widget = canvas.get_tk_widget()
-    canvas_widget.pack()
+        # Embedding the pie chart into the Tkinter window
+        canvas = FigureCanvasTkAgg(fig, master=root)
+        canvas_widget = canvas.get_tk_widget()
+        canvas_widget.pack()
 
 root = tk.Tk()
 root.title("Sentiment Analysis App")
