6804d6baf7a4affb4e73ed090af19ee7a66881c2
jurchiks33 <jurisslisans@gmail.com>
2023-12-16 20:28:33 +0100

    Adding language translator

--- VibeScan.py
+++ VibeScan.py
@@ -1,40 +1,62 @@
 import tkinter as tk
+from googletrans import Translator
 from nltk.sentiment import SentimentIntensityAnalyzer
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+translator = Translator()
+sia = SentimentIntensityAnalyzer()
 
 chart = None
 canvas = None
 
 def analyze_sentiment():
+    global chart, canvas
+
     text = text_input.get("1.0", "end-1c")
-    sia = SentimentIntensityAnalyzer()
-    sentiment = sia.polarity_scores(text)
-    result_label.config(text=str(sentiment))
+    
+    # Translate the text to English
+    translated_text = translator.translate(text, src='lv', dest='en').text
+
+    sentiment = sia.polarity_scores(translated_text)
+    result_label.config(text="Sentiment Analysis Results: " + str(sentiment))
 
     # Data for pie chart
     labels = ['Positive', 'Neutral', 'Negative']
     sizes = [sentiment['pos'], sentiment['neu'], sentiment['neg']]
     colors = ['green', 'blue', 'red']
 
-    # Update the pie chart with new data
-    if 'chart' in globals():
-        chart.set_sizes(sizes)
-        canvas.draw_idle()
-    else:
-        # Create figure for the pie chart
-        fig, ax = plt.subplots()
-        global chart, canvas
-        chart = ax.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=140)
-        ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    # Clear the previous pie chart
+    if chart is not None:
+        for piece in chart[0]:
+            piece.remove()
 
-        # Embedding the pie chart into the Tkinter window
-        canvas = FigureCanvasTkAgg(fig, master=root)
-        canvas_widget = canvas.get_tk_widget()
-        canvas_widget.pack()
+    # Create figure for the pie chart
+    fig, ax = plt.subplots()
+    chart = ax.pie(sizes, labels=labels, colors=colors, 
+                   autopct='%1.1f%%', startangle=140)
+    ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+    # Embedding the pie chart into the Tkinter window
+    if canvas is not None:
+        canvas.get_tk_widget().destroy()
+    canvas = FigureCanvasTkAgg(fig, master=root)
+    canvas_widget = canvas.get_tk_widget()
+    canvas_widget.pack()
 
 root = tk.Tk()
 root.title("Sentiment Analysis App")
+
+# Calculate window size and position
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+window_width = int(screen_width * 0.3)
+window_height = int(screen_height * 0.6)
+position_right = int(screen_width / 2 - window_width / 2)
+position_down = int(screen_height / 2 - window_height / 2)
+
+# Set window size and position
+root.geometry(f"{window_width}x{window_height}+{position_right}+{position_down}")
 
 text_input = tk.Text(root, height=10, width=50)
 text_input.pack()
